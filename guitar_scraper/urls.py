941fb45097c77e9e908202be6e94c4c40a6604ce
hpaasch <hepaasch@gmail.com>
2016-07-06 15:22:06 -0400

    detail view rough drawn.

--- guitar_scraper/urls.py
+++ guitar_scraper/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from tab_app.views import tab_search_view
+from tab_app.views import tab_detail_view, tab_search_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', tab_search_view, name='tab_search_view'),
+    url(r'^detail$', tab_detail_view, name='tab_detail_view'),
 
 ]
--- tab_app/views.py
+++ tab_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from urllib.parse import urlparse  # new library!!!
+import requests
+from bs4 import BeautifulSoup
+
 
 def tab_search_view(request):
-    return render(request, 'index.html', {})
+    pass
+
+
+def tab_detail_view(request):
+    url = 'http://www.guitartabs.cc/tabs/u/u2/with_or_without_you_tab.html'
+    content = requests.get(url).text  # grabs a string of html-y text
+    souper = BeautifulSoup(content, "html.parser")
+    results = str(souper.find(class_='tabcont'))
+    return render(request, 'index.html', {'results': results})
