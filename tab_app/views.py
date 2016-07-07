3fdc78b8f354e97091a81865f7b577279eb77952
hpaasch <hepaasch@gmail.com>
2016-07-07 08:41:23 -0400

    turning this in.

--- tab_app/views.py
+++ tab_app/views.py
@@ -9,10 +9,13 @@ def tab_search_view(request):
     content = requests.get(search_url).text
     souper = BeautifulSoup(content, 'html.parser')
     results = str(souper.find(class_='tabslist'))
-    pagination = str(souper.find(class_='paging'))  # this pagination doesn't work
+    pagination = str(souper.find(class_='paging')) # gets one page
     pager = souper.select('a[href^=?tabtype]')  # from here modifies the URL, but to what end?
-    for page in pager:
-        new_url = 'http://www.guitartabs.cc/search.php' + page['href']  # this is the correct URL, but how to get it into
+    # for page in pager:
+    #     new_url = 'http://www.guitartabs.cc/search.php' + page['href']  # this is the correct URL, but how to get it into
+    #     new_pagination = souper.find(class_='paging') # gets one page
+    #     pagination.append(new_pagination)
+    # pagination = str(pagination)
     return render(request, 'index.html', {'results': results, 'pagination': pagination})
 
 def tab_detail_view(request, url):
