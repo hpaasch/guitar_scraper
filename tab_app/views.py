f8907780071fca0a4fbde61ea4ea5efb78be7094
hpaasch <hepaasch@gmail.com>
2016-07-06 20:08:33 -0400

    mostly works.

--- tab_app/views.py
+++ tab_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from urllib.parse import urlparse  # new library!!!
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,23 +8,20 @@ def tab_search_view(request):
     search_url = 'http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}'.format(song)
     content = requests.get(search_url).text
     souper = BeautifulSoup(content, 'html.parser')
-    results = str(souper.find(class_='tabslist'))  # need for loop to get all results
-
-
-    # results = str(souper.find(id='song'))
-    # souper.find_all('a', class_='title')
-    # for link in links:
-    #     link_url = link.attrs['href']
-    #     print(urlparse(link_url).path)
-
-    return render(request, 'index.html', {'results': results})
-
+    results = str(souper.find(class_='tabslist'))
+    pagination = str(souper.find(class_='paging'))  # this pagination doesn't work
+    pager = souper.select('a[href^=?tabtype]')  # from here modifies the URL, but to what end?
+    for page in pager:
+        new_url = 'http://www.guitartabs.cc/search.php' + page['href']  # this is the correct URL, but how to get it into
+    return render(request, 'index.html', {'results': results, 'pagination': pagination})
 
 def tab_detail_view(request, url):
     song_url = 'http://www.guitartabs.cc/' + url
     content = requests.get(song_url).text  # grabs a string of html-y text
     souper = BeautifulSoup(content, "html.parser")
     results = str(souper.find(class_='tabcont'))
+    pagination = ''
     if results == 'None':
-        results = str(souper.find_all(class_='tabslist')[1])
-    return render(request, 'detail.html', {'results': results})
+        results = str(souper.find_all(class_='tabslist')[1])  # index the 2nd table
+        pagination = str(souper.find(class_='paging'))  #this pagination works great
+    return render(request, 'detail.html', {'results': results, 'pagination': pagination})
